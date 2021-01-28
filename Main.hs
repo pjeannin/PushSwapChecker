83bff295a56036f9d5769fbcaaa195b9f35401c0
Pierre Jeannin <pierre.jeannin@epitech.eu>
2021-01-28 11:14:21 +0100

    add check args

--- Main.hs
+++ Main.hs
@@ -6,7 +6,37 @@
 --
 
 import System.Environment
+import Data.Maybe
+
+containOnlyInt :: [Char] -> Bool
+containOnlyInt [] = False
+containOnlyInt [x]
+        | x >= '0' && x <= '9' = True
+        | otherwise = False
+containOnlyInt (x:xs)
+        | x >= '0' && x <= '9' = containOnlyInt xs
+        | otherwise = False
+
+readInt :: [Char] -> Maybe Int
+readInt [] = Nothing
+readInt [x]
+        | containOnlyInt [x] = Just (read [x]::Int)
+        | otherwise = Nothing
+readInt (x:xs)
+        | containOnlyInt (x:xs) = Just (read (x:xs)::Int)
+        | otherwise = Nothing
+
+
+checkArgs :: [String] -> Maybe [Int]
+checkArgs [] = Nothing
+checkArgs [x]
+        | readInt x == Nothing = Nothing
+        | otherwise = Just [fromJust (readInt x)]
+checkArgs (x:xs)
+        | readInt x == Nothing = Nothing
+        | checkArgs xs == Nothing = Nothing
+        | otherwise = Just (fromJust (readInt x) : fromJust (checkArgs xs))
 
 main = do
-    args <- getArgs
-    print args
+        args <- checkArgs getArgs
+        print args
